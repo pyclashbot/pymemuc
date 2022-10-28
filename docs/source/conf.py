c5f63553d0083726baf43f4b137f2e491961530d
Martin Miglio <marmig0404@gmail.com>
2022-10-27 21:38:49 -0400

    add sphinx extensions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,10 +13,7 @@ author = "Martin Miglio"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.autosummary"
-]
+extensions = ['sphinx.ext.todo', 'sphinx.ext.viewcode', 'sphinx.ext.autodoc']
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
