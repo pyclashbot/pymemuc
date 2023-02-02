92896eeaf5b9818402a5e90107cf6208daba3612
Martin Miglio <marmig0404@gmail.com>
2023-02-01 23:11:05 -0500

    switch to furo theme
    update rtd build workflow

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,7 +7,7 @@ sys.path.insert(0, os.path.abspath("../.."))
 
 
 project = "pymemuc"
-copyright = "2022, Martin Miglio"  # noqa
+copyright = "2022, Martin Miglio"  # noqa #pylint: disable=redefined-builtin
 author = "Martin Miglio"
 
 extensions = [
@@ -16,6 +16,8 @@ extensions = [
     "sphinx.ext.viewcode",
     "sphinx.ext.autodoc",
     "sphinx.ext.viewcode",
+    "sphinxext.opengraph",
+    "sphinx_copybutton",
 ]
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
@@ -26,7 +28,7 @@ autodoc_default_options = {
 add_module_names = False
 
 
-html_theme = "classic"
+html_theme = "furo"
 html_theme_options = {
     "sidebarwidth": "19em",
 }
